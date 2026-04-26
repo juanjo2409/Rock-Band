4ff833b3e8bcbc6d4147a12af536716a336d75ca
juan <juanjosemn160@gmail.com>
2026-04-26 10:19:54 -0500

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+const hamburguesa =
+document.getElementById("hamburguesa");
+
+const menu =
+document.getElementById("menu");
+
+hamburguesa.addEventListener("click", function(){
+
+menu.classList.toggle("activo");
+
+});
+
+});
